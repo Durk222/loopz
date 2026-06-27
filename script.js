7c794a92af8d65d101f22b56bcf1a0a55df360df
Franciswow <carvalloalan005@gmail.com>
2026-06-26 21:02:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+/* ==========================================================================
+   LOOPZ FRONTEND LOGIC & POCKETBASE CONNECTION
+   Este archivo manejará la API con Pockethost y el algoritmo del Feed.
+   ========================================================================== */
+
+console.log("Sistema Loopz inicializado. Esperando conexión al backend...");
